f4b941d583a1cd8d0fc37d73d8a9cc133bf49f64
Guilherme Faria <guilherme_fgl@hotmail.com>
2021-01-21 11:53:26 -0300

    rest api for tests

---
+++ producer/src/main/java/com/example/spring/producer/api/rest/AmqpApi.java
@@ -0,0 +1,25 @@
+package com.example.spring.producer.api.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.spring.producer.dto.MessageQueue;
+import com.example.spring.producer.service.AmqpService;
+
+@RestController("api")
+public class AmqpApi {
+
+	@Autowired
+	private AmqpService service;
+
+	@PostMapping("/send")
+	@ResponseStatus(HttpStatus.ACCEPTED)
+	public void sendToConsumer(@RequestBody MessageQueue message) {
+		service.sendToConsumer(message);
+	}
+
+}
